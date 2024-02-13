846532b8cce0a8193ed8cfaf86a89c492d287cb0
Fabrizzio Rios <warmachine860@gmail.com>
2024-02-12 19:35:37 -0600

    Json

--- netmiko_automation_tools.py
+++ netmiko_automation_tools.py
@@ -1,9 +1,11 @@
+import json
+
 from netmiko import ConnectHandler
 
 
 class Device:
     @classmethod
-    def create_connection_send_command(cls, device: dict, command: str):
+    def create_connection_send_command(cls, device: dict, command: str) -> bool:
         try:
             device_connection = ConnectHandler(**device)
             device_connection.send_config_set(command)
@@ -13,23 +15,28 @@ class Device:
             return False
 
     @classmethod
-    def conf_device(cls, device: dict, config_archive: str):
+    def conf_device(cls, device: dict, config_archive: str) -> bool:
         try:
             device_connection = ConnectHandler(**device)
             device_connection.send_config_from_file(config_archive)
             device_connection.disconnect()
             return True
-
         except Exception as err:
             return False
 
 
 class Tools:
     @classmethod
-    def find_device(cls, list_to_search: list, key_to_search: str,
-                    element_to_search: str):
+    def open_json_from_file(cls, file_path: str) -> list:
+        with open(file_path, 'r') as file:
+            json_data = json.load(file)
+        return json_data
+
+    @classmethod
+    def find_element_in_list(cls, list_to_search: list, key_to_search: str,
+                             element_to_search: str):
         try:
-            device_find = next(item for item in list_to_search if item[key_to_search] == element_to_search)
-            return device_find
+            element_found = next(item for item in list_to_search if item[key_to_search] == element_to_search)
+            return element_found
         except Exception as err:
             return None
